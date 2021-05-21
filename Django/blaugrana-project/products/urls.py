ada0c26b02b4fa8ecacf5cbb7d1ba49beac07882
Abhisar Saraswat <abhisarsaraswat@gmail.com>
2021-05-21 16:10:38 +0530

    Updated

--- Django/blaugrana-project/products/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<int:product_id>', views.detail, name='detail'),
-    path('<slug:type>', views.category, name='category'),
-]
